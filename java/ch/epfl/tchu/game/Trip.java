a9bd9ced175aca85f6675357cfa8d45fcb354fb1
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2021-02-22 20:04:04 +0100

    Update Trip.java

--- java/ch/epfl/tchu/game/Trip.java
+++ java/ch/epfl/tchu/game/Trip.java
@@ -1,6 +1,60 @@
 package ch.epfl.tchu.game;
 
-public class Trip
-{
-    //TODO IMPLEMENT
-}
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+
+public final class Trip {
+	  private final Station from;
+	  private final Station to;
+	  private int points;
+	  
+	  
+
+	  public Trip(Station from, Station to, int points) { 
+		  
+	    this.from = Objects.requireNonNull(from);
+	    this.to = Objects.requireNonNull(to);
+	    this.points = points;
+	    
+	  }
+	
+	public static final List<Trip> all(List<Station> from, List<Station> to, int points) {
+		
+		
+		
+		
+	}
+
+	public Station from() {
+		
+		return from;	
+	}
+	
+	public Station to() {
+		
+		return to;
+	}
+	
+	public int points() {
+		if(points >= 0) {
+			
+			return points;
+			 
+		}
+		else {
+			
+			throw new IllegalArgumentException();
+		}
+	
+	}
+	
+	public int points(StationConnectivity connectivity) {
+		
+		return 0;
+			
+	}
+	
+	
+  }
