a180a4a8f1b08a4346efdb9634276613c0ccb996
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2021-02-22 20:05:53 +0100

    Update Station.java

--- java/ch/epfl/tchu/game/Station.java
+++ java/ch/epfl/tchu/game/Station.java
@@ -1,6 +1,39 @@
 package ch.epfl.tchu.game;
 
-public class Station
-{
-    //TODO IMPLEMENT
+import ch.epfl.tchu.Preconditions;
+
+
+public final class Station {
+	
+	int id;
+	String name;
+	
+
+	public Station(int id, String name) {
+		
+		if(id >= 0) {
+			
+			this.id = id();
+			this.name = name().toString();		
+		}
+		else {
+		
+			throw new IllegalArgumentException();
+	     
+		}
+	}
+	
+      public int id() {
+		
+    	  return id;
+ 	
+		}
+          
+      public String name() {
+    	  
+		return name;
+		
+	}	
+	
 }
+
