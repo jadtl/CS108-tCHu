b108add429a0835bcaa84fbdfc28004065656476
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2021-02-23 17:23:25 +0100

    Update Card.java

--- java/ch/epfl/tchu/game/Card.java
+++ java/ch/epfl/tchu/game/Card.java
@@ -3,8 +3,8 @@ package ch.epfl.tchu.game;
 import java.util.Arrays;
 import java.util.List;
 
-public enum Card {
-
+public enum Card
+{
     BLACK,
     VIOLET,
     BLUE,
@@ -16,36 +16,51 @@ public enum Card {
     LOCOMOTIVE;
 
 
-
+	
     public static final List<Card> ALL = Arrays.asList(Card.values());
+
     public static final int COUNT = ALL.size();
-
-    private Color color;
-
-
-
-    Card()
-    {
-        color = this.toString().equals("LOCOMOTIVE") ? null : Color.valueOf(this.toString());
-    }
 
     // same list as ALL, except without LOCOMOTIVE
     public static final List<Card> CARS = ALL.subList(0, ALL.size() - 1);
-
-    public static Card of(Color color) {
-
-        for (Card card : CARS) {
+    
+    /**
+     * returns type of wagon 
+     * corresponding to color
+     */
+    
+    public static Card of(Color color)
+    {
+        for (Card card : CARS)
+        {
             // return current card if equal to given color
             if (card.toString().equals(color.toString()))
                 return card;
         }
-
         return null;
     }
+    
+    /**
+     * @param color
+     */
+
+    private Color color;
 
     public Color color()
     {
         return color;
     }
+    
+    /**
+     * Returns the card color 
+     * for a wagon and null for
+     * a locomotive (have no color)
+     */
 
+
+     Card()
+    {
+        color = this.toString().equals("LOCOMOTIVE") ? null : Color.valueOf(this.toString());
+    }
 }
+
