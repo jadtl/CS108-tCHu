ec52b6a6e067ace1209b2441b617d39461caef46
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2021-02-23 17:26:18 +0100

    Update StationConnectivity.java

--- java/ch/epfl/tchu/game/StationConnectivity.java
+++ java/ch/epfl/tchu/game/StationConnectivity.java
@@ -1,5 +1,9 @@
 package ch.epfl.tchu.game;
 
+/**
+ * method establishes if 2 stations are connected
+ */
+
 public interface StationConnectivity {
 
 	public abstract boolean connected(Station s1, Station s2);
