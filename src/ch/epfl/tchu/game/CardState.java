cb31c4a7697b35aa00ec80fbf0333ff660dcb3d9
Jad Tala <56200813+DiligentNetrunner@users.noreply.github.com>
2021-03-09 03:57:00 +0100

    Implemented card state javadoc

--- src/ch/epfl/tchu/game/CardState.java
+++ src/ch/epfl/tchu/game/CardState.java
@@ -15,40 +15,47 @@ import ch.epfl.tchu.SortedBag;
  * @author Jad Tala (310821)
  */
 public final class CardState extends PublicCardState {
-	private final Deck<Card> deckCards;
+	private final Deck<Card> deck;
 	private final SortedBag<Card> discardedCards;
 
 	/**
+	 * Constructs a card state from face-up cards, a deck and discards
 	 * 
 	 * @param faceUpCards
-	 * @param deckCards
+	 * @param deck
 	 * @param discardedCards
 	 */
-	private CardState(List<Card> faceUpCards, Deck<Card> deckCards, SortedBag<Card> discardedCards) {
-		super(faceUpCards, deckCards.size(), discardedCards.size());
+	private CardState(List<Card> faceUpCards, Deck<Card> deck, SortedBag<Card> discardedCards) {
+		super(faceUpCards, deck.size(), discardedCards.size());
 
-		this.deckCards = deckCards;
+		this.deck = deck;
 		this.discardedCards = discardedCards;
 	}
 
 	/**
+	 * Returns a new card state from a deck
 	 * 
-	 * @param deck
+	 * @param deck a deck of cards
 	 * 
-	 * @return
+	 * @return a new card state from a deck
+	 * 
+	 * @throws IllegalArgumentException if the deck size is below the required face-up cards size
 	 */
 	public static CardState of(Deck<Card> deck) {
-		Preconditions.checkArgument(deck.size() >= 5);
+		Preconditions.checkArgument(deck.size() >= Constants.FACE_UP_CARDS_COUNT);
 
 		return new CardState(deck.topCards(Constants.FACE_UP_CARDS_COUNT).toList(),
 				deck.withoutTopCards(Constants.FACE_UP_CARDS_COUNT), new SortedBag.Builder<Card>().build());
 	}
 
 	/**
+	 * Returns a card state for which the chosen face-up card is replaced by the top deck card and discarded
 	 * 
-	 * @param slot
+	 * @param slot the index of the drawn face-up card
 	 * 
-	 * @return
+	 * @return a card state for which the chosen face-up card is replaced by the top deck card and discarded
+	 * 
+	 * @throws IndexOutOfBounds if slot is not within faceUpCards range
 	 */
 	CardState withDrawnFaceUpCard(int slot) {
 		Objects.checkIndex(slot, Constants.FACE_UP_CARDS_COUNT);
@@ -58,41 +65,47 @@ public final class CardState extends PublicCardState {
 		List<Card> faceUpCards = List.copyOf(faceUpCards());
 		faceUpCards.set(slot, topDeckCard());
 
-		return new CardState(faceUpCards, deckCards.withoutTopCard(), updatedDiscardedCards);
+		return new CardState(faceUpCards, deck.withoutTopCard(), updatedDiscardedCards);
 	}
 
 	/**
+	 * Returns the card on top of the deck
 	 * 
-	 * @return
+	 * @return the card on top of the deck
 	 */
-	public Card topDeckCard() { return deckCards.topCard(); }
+	public Card topDeckCard() { return deck.topCard(); }
 
 	/**
+	 * Returns a card state for which the deck top card has been removed
 	 * 
-	 * @return
+	 * @return a card state for which the deck top card has been removed
 	 */
-	public CardState withoutTopDeckCard() { return new CardState(faceUpCards(), deckCards.withoutTopCard(), discardedCards); }
+	public CardState withoutTopDeckCard() { return new CardState(faceUpCards(), deck.withoutTopCard(), discardedCards); }
 
 	/**
+	 * Returns a card state for which the empty deck has been replaced by the discards, shuffled
 	 * 
-	 * @param rng
+	 * @param rng a random number generator
 	 * 
-	 * @return
+	 * @return a card state for which the empty deck has been replaced by the discards, shuffled
+	 * 
+	 * @throws IllegalArgumentException if the deck is not empty
 	 */
 	public CardState withDeckRecreatedFromDiscards(Random rng) {
-		Preconditions.checkArgument(deckCards.size() == 0);
+		Preconditions.checkArgument(isDeckEmpty());
 		
 		return new CardState(faceUpCards(), Deck.of(discardedCards, rng), new SortedBag.Builder<Card>().build());
 	}
 
 	/**
+	 * Returns the card state for which additionalDiscards has been added to its discards
 	 * 
-	 * @param additionalDiscards
+	 * @param additionalDiscards the cards to add to the discards
 	 * 
-	 * @return
+	 * @return the card state for which additionalDiscards has been added to its discards
 	 */
 	public CardState withMoreDiscardedCards(SortedBag<Card> additionalDiscards) { 
-		return new CardState(faceUpCards(), deckCards, new SortedBag.Builder<Card>()
+		return new CardState(faceUpCards(), deck, new SortedBag.Builder<Card>()
 		.add(discardedCards).add(additionalDiscards).build());
 	}
 }
