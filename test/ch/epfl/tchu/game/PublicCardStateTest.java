f2213e9db5c1cf1aad21128d05702ac12d318c5e
Jad Tala <JadTala1621@gmail.com>
2021-03-09 12:30:06 +0100

    faceUpCard test for PublicStateCard

--- test/ch/epfl/tchu/game/PublicCardStateTest.java
+++ test/ch/epfl/tchu/game/PublicCardStateTest.java
@@ -25,4 +25,19 @@ class PublicCardStateTest {
 
     assertEquals(faceUpCards.size() + deckSize + 0, publicCardState.totalSize());
   }
+
+  @Test
+
+  void faceUpCardWorks() {
+    List<Card> faceUpCards = List.of(Card.RED, Card.WHITE, Card.BLACK, Card.LOCOMOTIVE, Card.VIOLET);
+    int deckSize = 10;
+    int discardsSize = 0;
+    PublicCardState publicCardState = new PublicCardState(faceUpCards, deckSize, discardsSize);
+
+    assertEquals(Card.RED, publicCardState.faceUpCard(0));
+    assertEquals(Card.WHITE, publicCardState.faceUpCard(1));
+    assertEquals(Card.BLACK, publicCardState.faceUpCard(2));
+    assertEquals(Card.LOCOMOTIVE, publicCardState.faceUpCard(3));
+    assertEquals(Card.VIOLET, publicCardState.faceUpCard(4));
+  }
 }
