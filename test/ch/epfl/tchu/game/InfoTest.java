132b248b35ff6403ec70b8d21b3835a38984f9d6
Jad Tala <JadTala1621@gmail.com>
2021-03-09 23:39:13 +0100

    Added info test with to do item

---
+++ test/ch/epfl/tchu/game/InfoTest.java
@@ -0,0 +1,12 @@
+package ch.epfl.tchu.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InfoTest {
+  @Test
+  void test() {
+    // TODO Implement info tests
+  }
+}
