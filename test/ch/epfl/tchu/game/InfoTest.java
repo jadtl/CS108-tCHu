58c9ba0f45c2c285f6c0d4371cd75e892a6ec33b
Jad Tala <JadTala1621@gmail.com>
2021-03-10 20:33:58 +0100

    Added static methods tests for info

--- test/ch/epfl/tchu/game/InfoTest.java
+++ test/ch/epfl/tchu/game/InfoTest.java
@@ -2,11 +2,24 @@ package ch.epfl.tchu.game;
 
 import org.junit.jupiter.api.Test;
 
+import ch.epfl.tchu.gui.Info;
+import ch.epfl.tchu.gui.StringsFr;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 class InfoTest {
   @Test
-  void test() {
-    // TODO Implement info tests
+  void cardNameWorks() {
+    assertEquals("noires", Info.cardName(Card.BLACK, 2));
+  }
+
+  @Test
+  void drawWorks() {
+    String playerName = "Elon Musk";
+    String opponentName = "Jeff Bezos";
+
+    assertEquals("\nElon Musk et Jeff Bezos sont ex æqo avec 10 points !\n", Info.draw(List.of(playerName, opponentName), 10));
   }
 }
