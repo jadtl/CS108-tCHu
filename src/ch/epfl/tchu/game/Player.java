7a5c036c70b4c06b00df2c372e3262aaeb66772e
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2021-03-20 20:47:37 +0100

    Update Player.java

--- src/ch/epfl/tchu/game/Player.java
+++ src/ch/epfl/tchu/game/Player.java
@@ -1,7 +1,39 @@
 package ch.epfl.tchu.game;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import ch.epfl.tchu.SortedBag;
+
 public interface Player {
-  enum TurnKind {
-    DRAW_TICKETS;
-  }
+	enum TurnKind {
+		DRAW_TICKETS, DRAW_CARDS, CLAIM_ROUTE;
+
+	public static final List<TurnKind> ALL = Arrays.asList(TurnKind.values());
+	}
+	
+	public void initPlayers(PlayerId ownId, Map<PlayerId, String> playerNames);
+	
+	void receiveInfo(String info);
+	
+	void updateState(PublicGameState newState, PlayerState ownState);
+	
+	void setInitialTicketChoice(SortedBag<Ticket> tickets);
+	
+	SortedBag<Ticket> chooseInitialTickets();
+	
+	TurnKind nextTurn();
+	
+	SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options);
+	
+	int drawSlot();
+	
+	Route claimedRoute();
+	
+	SortedBag<Card> initialClaimCards();
+	
+	SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options);
+	
+
 }
