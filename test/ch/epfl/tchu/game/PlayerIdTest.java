c7ca8447509690555fe740be67d71e63c6f5eee3
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2021-03-22 17:42:03 +0100

    Update PlayerIdTest.java

--- test/ch/epfl/tchu/game/PlayerIdTest.java
+++ test/ch/epfl/tchu/game/PlayerIdTest.java
@@ -2,12 +2,19 @@ package ch.epfl.tchu.game;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PlayerIdTest {
-  @Test
-  void playerIdNextWorks() {
-    assertEquals(PlayerId.PLAYER_2, PlayerId.PLAYER_1.next());
-    assertEquals(PlayerId.PLAYER_1, PlayerId.PLAYER_2.next());
-  }
+class PlayerIdTest {
+    @Test
+    void playerIdAllIsDefinedCorrectly() {
+        assertEquals(List.of(PlayerId.PLAYER_1, PlayerId.PLAYER_2), PlayerId.ALL);
+    }
+
+    @Test
+    void playerIdNextWorks() {
+        assertEquals(PlayerId.PLAYER_2, PlayerId.PLAYER_1.next());
+        assertEquals(PlayerId.PLAYER_1, PlayerId.PLAYER_2.next());
+    }
 }
