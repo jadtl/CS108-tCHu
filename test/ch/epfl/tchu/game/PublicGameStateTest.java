9c4b9c15f3361d58e7a2c08316bfca28f8a1098a
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2021-03-22 19:05:01 +0100

    Create PublicGameStateTest.java

---
+++ test/ch/epfl/tchu/game/PublicGameStateTest.java
@@ -0,0 +1,40 @@
+package ch.epfl.tchu.game;
+
+import org.junit.jupiter.api.Test;
+
+
+public class PublicGameStateTest {
+
+//TODO Test that it works with null and non  null Last Player
+
+	@Test
+
+	void PublicGameStateTestWithNullLastPlayer() {
+
+	}
+
+//TODO Test that it only works if deck + discards = 5
+
+	@Test
+
+	void PublicGameStateCanDrawCardsTest() {
+
+	}
+//TODO Test it returns the routes current player possesses
+
+	@Test
+
+	void PublicGameStateClaimedRoutes() {
+
+	}
+
+//TODO Test it returns Null if the last turn has not started yet
+//TODO Test it does not return Null otherwise
+
+	@Test
+
+	void PublicGameStateLastPlayer() {
+
+	}
+
+}
