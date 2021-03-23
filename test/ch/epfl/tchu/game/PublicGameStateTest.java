30eb1d7565ea9cd6bbf8f12bd81e4f0306f640b5
Jad Tala <JadTala1621@gmail.com>
2021-03-23 20:22:42 +0100

    Tested PublicGameState

--- test/ch/epfl/tchu/game/PublicGameStateTest.java
+++ test/ch/epfl/tchu/game/PublicGameStateTest.java
@@ -1,40 +1,51 @@
 package ch.epfl.tchu.game;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 import org.junit.jupiter.api.Test;
+
+import ch.epfl.tchu.SortedBag;
 
 
 public class PublicGameStateTest {
 
-//TODO Test that it works with null and non  null Last Player
-
 	@Test
-
 	void PublicGameStateTestWithNullLastPlayer() {
-
+		assertDoesNotThrow(() -> { new PublicGameState(0, new PublicCardState(SortedBag.of(5, Card.BLACK).toList(), 7, 0), PlayerId.PLAYER_1, Map.of(PlayerId.PLAYER_1, new PublicPlayerState(0, 0, List.of()), PlayerId.PLAYER_2, new PublicPlayerState(0, 0, List.of())), null);});
 	}
 
-//TODO Test that it only works if deck + discards = 5
-
 	@Test
-
 	void PublicGameStateCanDrawCardsTest() {
+		PublicGameState gameState = new PublicGameState(0, new PublicCardState(SortedBag.of(5, Card.BLACK).toList(), 7, 0), PlayerId.PLAYER_1, Map.of(PlayerId.PLAYER_1, new PublicPlayerState(0, 0, List.of()), PlayerId.PLAYER_2, new PublicPlayerState(0, 0, List.of())), null);
 
+		assertTrue(gameState.canDrawCards());
+
+		gameState = new PublicGameState(0, new PublicCardState(SortedBag.of(5, Card.BLACK).toList(), 3, 2), PlayerId.PLAYER_1, Map.of(PlayerId.PLAYER_1, new PublicPlayerState(0, 0, List.of()), PlayerId.PLAYER_2, new PublicPlayerState(0, 0, List.of())), null);
+
+		assertTrue(gameState.canDrawCards());
+
+		gameState = new PublicGameState(0, new PublicCardState(SortedBag.of(5, Card.BLACK).toList(), 1, 2), PlayerId.PLAYER_1, Map.of(PlayerId.PLAYER_1, new PublicPlayerState(0, 0, List.of()), PlayerId.PLAYER_2, new PublicPlayerState(0, 0, List.of())), null);
+
+		assertFalse(gameState.canDrawCards());
 	}
-//TODO Test it returns the routes current player possesses
 
 	@Test
-
 	void PublicGameStateClaimedRoutes() {
+		PublicGameState gameState = new PublicGameState(0, new PublicCardState(SortedBag.of(5, Card.BLACK).toList(), 3, 2), PlayerId.PLAYER_1, Map.of(PlayerId.PLAYER_1, new PublicPlayerState(0, 0, List.of()), PlayerId.PLAYER_2, new PublicPlayerState(0, 0, List.of())), null);
 
+		assertTrue(gameState.claimedRoutes().isEmpty());
 	}
-
-//TODO Test it returns Null if the last turn has not started yet
-//TODO Test it does not return Null otherwise
 
 	@Test
-
 	void PublicGameStateLastPlayer() {
+		PublicGameState gameState = new PublicGameState(0, new PublicCardState(SortedBag.of(5, Card.BLACK).toList(), 3, 2), PlayerId.PLAYER_1, Map.of(PlayerId.PLAYER_1, new PublicPlayerState(0, 0, List.of()), PlayerId.PLAYER_2, new PublicPlayerState(0, 0, List.of())), null);
 
+		assertTrue(Objects.isNull(gameState.lastPlayer()));
 	}
-
 }
