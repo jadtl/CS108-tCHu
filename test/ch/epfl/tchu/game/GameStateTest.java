0011342d3321a6d401f24081a6904ec7a52573eb
Jad Tala <JadTala1621@gmail.com>
2021-03-24 10:05:10 +0100

    GameState constructor test

--- test/ch/epfl/tchu/game/GameStateTest.java
+++ test/ch/epfl/tchu/game/GameStateTest.java
@@ -1,16 +1,25 @@
 package ch.epfl.tchu.game;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 
+import ch.epfl.tchu.SortedBag;
+import java.util.Random;
+
 public class GameStateTest {
-
-//TODO Test that the 8 cards distributed to the players are substracted from the Card Deck 
-//TODO Test that the 2 initial decks are randomly mixed
-
+	
 	@Test
+	public void GameStateInitialTest() {
+		Station LAU = new Station(0, "Lausanne");
+		Station EPF = new Station(1, "EPFL");
+		GameState gameState = GameState.initial(SortedBag.of(5, new Ticket(List.of(new Trip(LAU, EPF, 10)))), new Random());
 
-	void GameStateInitialTest() {
-
+		assertEquals(5, gameState.ticketsCount());
+		assertEquals(Constants.ALL_CARDS.size() - 2 * Constants.INITIAL_CARDS_COUNT, gameState.cardState().totalSize());
+		assertEquals(Constants.INITIAL_CARDS_COUNT, gameState.playerState(PlayerId.PLAYER_1).cardCount());
+		assertEquals(Constants.INITIAL_CARDS_COUNT, gameState.playerState(PlayerId.PLAYER_2).cardCount());
 	}
-
 }
