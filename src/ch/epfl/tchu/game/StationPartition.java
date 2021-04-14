c87f2492f667b256d3ad1587b2e1f9c4049258ea
Jad Tala <JadTala1621@gmail.com>
2021-04-14 17:44:40 +0000

    Formatted station partition

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -21,11 +21,10 @@ public final class StationPartition implements StationConnectivity {
 
   @Override
   public boolean connected(Station s1, Station s2) {
-    if (s1.id() >= stationConnectivity.length || s2.id() >= stationConnectivity.length) {
+    if (s1.id() >= stationConnectivity.length || s2.id() >= stationConnectivity.length)
       return s1.id() == s2.id();
-    } else {
+    else
       return stationConnectivity[s1.id()] == stationConnectivity[s2.id()];
-    }
   }
 
   /**
