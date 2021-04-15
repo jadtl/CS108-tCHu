df81a5e4cf01a182516cb84f90e699955372dbee
Jad Tala <JadTala1621@gmail.com>
2021-04-15 09:26:46 +0000

    Documented end of the game

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -1,11 +1,11 @@
 package ch.epfl.tchu.game;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
@@ -20,6 +20,8 @@ import ch.epfl.tchu.gui.Info;
  * @author Jad Tala (310821)
  */
 public final class Game {
+	private static final int DRAW_CARDS_TIMES = 2;
+
 	/**
 	 * Makes the given players play a game of tCHu, whose names and identifiers figure in the given maps
 	 * 
@@ -39,23 +41,24 @@ public final class Game {
 	 *         If one of the two maps has a size different from 2
 	 */
 	public static void play(Map<PlayerId, Player> players, Map<PlayerId, String> playerNames, SortedBag<Ticket> tickets, Random rng) {
-		Preconditions.checkArgument(players.size() == 2 && playerNames.size() == 2);
+		Preconditions.checkArgument(players.size() == PlayerId.COUNT && playerNames.size() == PlayerId.COUNT);
 
-		/*
-		 * Start of the game
-		 */
+		/* * * * * * * * * *
+		 * The Early Game  *
+		 * * * * * * * * * */
+
 		// Initialiazing every player
-		PlayerId.ALL.stream().forEach((PlayerId playerId) -> { 
-			players.get(playerId).initPlayers(playerId, playerNames); 
-		});
+		PlayerId.ALL.stream()
+		.forEach(p -> players.get(p).initPlayers(p, playerNames));
 
 		// Generating the initial game state
 		GameState gameState = GameState.initial(tickets, rng);
-		// A map that links a player's identifier to their info class instance
-		Map<PlayerId, Info> playerInfos = new HashMap<>();
-		for (PlayerId playerId : PlayerId.ALL) {
-			playerInfos.put(playerId, new Info(playerNames.get(playerId)));
-		}
+
+		// Declaring a map that links a player's identifier to their info class instance
+		Map<PlayerId, Info> playerInfos = PlayerId.ALL.stream()
+		.collect(Collectors.toMap(p -> p, p -> new Info(playerNames.get(p))));
+		
+		// Informing the players which plays first
 		sendInfo(players,playerInfos.get(gameState.currentPlayerId()).willPlayFirst());
 
 		// Drawing initial tickets and asking the player which they want to keep
@@ -65,16 +68,20 @@ public final class Game {
 			updatePlayerStates(players, gameState);
 			gameState = gameState.withInitiallyChosenTickets(PlayerId.ALL.get(i), players.get(PlayerId.ALL.get(i)).chooseInitialTickets());
 		}
+
 		// Informing every player of the number of initially drawn tickets only once everyone has drawn
 		for (PlayerId playerId : PlayerId.ALL) {
 			sendInfo(players, playerInfos.get(playerId).keptTickets(gameState.currentPlayerState().ticketCount()));
 		}
+
 		// Updating the game state after having drawn the initial tickets
 		gameState = gameState.withoutTopTickets(Constants.INITIAL_TICKETS_COUNT * PlayerId.COUNT);
 		
-		/*
-		 * Middle of the game
-		 */
+		/* * * * * * * * * *
+		 * The Middle Game *
+		 * * * * * * * * * */
+
+		// Keep playing until the last turn is played
 		while (Objects.isNull(gameState.lastPlayer()) || gameState.currentPlayerId() != gameState.lastPlayer()) {
 			if (gameState.lastTurnBegins()) sendInfo(players, playerInfos.get(gameState.currentPlayerId()).lastTurnBegins(gameState.currentPlayerState().carCount()));
 			gameState = gameState.forNextTurn();
@@ -93,7 +100,7 @@ public final class Game {
 					break;
 
 				case DRAW_CARDS:
-					for (int i = 0; i < 2; i++) { //TODO Add constant
+					for (int i = 0; i < DRAW_CARDS_TIMES; i++) {
 						updatePlayerStates(players, gameState);
 						int drawSlot = currentPlayer.drawSlot();
 						if (drawSlot == Constants.DECK_SLOT) {
@@ -162,54 +169,76 @@ public final class Game {
 			}
 		}
 	  
-		/*
-		 * End of the game
-		 */
+		/* * * * * * * * *
+		 * The End Game  *
+		 * * * * * * * * */
+
+		// Letting the players know how the game effectively ended
 		updatePlayerStates(players, gameState);
-		Map<PlayerId, Integer> playersScores = new HashMap<PlayerId, Integer>();
-		Map<PlayerId, Trail> playersLongestTrail = new HashMap<PlayerId, Trail>();
-		Integer longestTrailLength = 0;
 
-		for (PlayerId playerId : PlayerId.ALL) {
-			playersScores.put(playerId, gameState.playerState(playerId).finalPoints());
-			Trail currentPlayerLongestTrail = Trail.longest(gameState.playerState(playerId).routes());
-			playersLongestTrail.put(playerId, currentPlayerLongestTrail);
-			longestTrailLength = Math.max(longestTrailLength, currentPlayerLongestTrail.length());
-		}
+		// Declaring an effectively final game state to use in lambdas
+		GameState gameStateFinal = gameState;
 
-		List<PlayerId> bonusEarningPlayers = new ArrayList<PlayerId>();
-		for (PlayerId playerId : PlayerId.ALL) {
-			if (playersLongestTrail.get(playerId).length() == longestTrailLength)
-				bonusEarningPlayers.add(playerId); 
-		}
+		// Declaring a map to keep track of the players' scores
+		Map<PlayerId, Integer> playersScores = PlayerId.ALL.stream()
+		.collect(Collectors.toMap(p -> p, p -> gameStateFinal.playerState(p).finalPoints()));
 
-		for (PlayerId playerId : bonusEarningPlayers) {
-			Integer newScore = playersScores.get(playerId) + Constants.LONGEST_TRAIL_BONUS_POINTS;
-			playersScores.replace(playerId, newScore);
-			sendInfo(players, playerInfos.get(playerId).getsLongestTrailBonus(playersLongestTrail.get(playerId)));
-		}
+		// Computing the longest trail among the players'
+		Integer longestTrailLength = PlayerId.ALL.stream()
+		.map(p -> Trail.longest(gameStateFinal.playerState(p).routes()).length())
+		.max(Integer::compare)
+		.get();
+
+		// Determining the players who earn the bonus for the longest trail
+		List<PlayerId> bonusEarningPlayers = PlayerId.ALL.stream()
+		.filter(p -> Trail.longest(gameStateFinal.playerState(p).routes()).length() == longestTrailLength)
+		.collect(Collectors.toList());
+
+		// Awarding the eligible players the bonus points and informing them
+		bonusEarningPlayers.stream()
+		.forEach(p -> { 
+			playersScores.replace(p, playersScores.get(p) + Constants.LONGEST_TRAIL_BONUS_POINTS);
+			sendInfo(players, playerInfos.get(p).getsLongestTrailBonus(Trail.longest(gameStateFinal.playerState(p).routes()))); 
+		});
 		
-		Integer highestScore = 0;
-		for (PlayerId playerId : PlayerId.ALL)
-			highestScore = Math.max(playersScores.get(playerId), highestScore);
+		// Getting the highest score from all players
+		Integer highestScore = PlayerId.ALL.stream()
+		.map((PlayerId playerId) -> playersScores.get(playerId))
+		.max(Integer::compareTo).get();
 
-		List<PlayerId> winners = new ArrayList<PlayerId>();
-		for (PlayerId playerId : PlayerId.ALL)
-			if (playersScores.get(playerId) == highestScore)
-				winners.add(playerId);
+		// Getting the players who got the highest score
+		List<PlayerId> winners = PlayerId.ALL.stream()
+		.filter((PlayerId playerId) -> playersScores.get(playerId) == highestScore)
+		.collect(Collectors.toList());
 
-		// Note: This code only works for 2 players
-		if (winners.size() == 1)	sendInfo(players, playerInfos.get(winners.get(0)).won(playersScores.get(winners.get(0)), playersScores.get(winners.get(0).next())));	
+		// Breaking down the game, declaring either a win for a player or a draw
+		if (winners.size() == 1) sendInfo(players, playerInfos.get(winners.get(0)).won(playersScores.get(winners.get(0)), playersScores.get(winners.get(0).next())));
 		else sendInfo(players, Info.draw(new ArrayList<String>(playerNames.values()), highestScore));
 	}
 
+	/**
+	 * Transmits to every player a given information
+	 * 
+	 * @param players
+	 * 				The map linking players to their identifier
+	 * 
+	 * @param info
+	 * 				The information to transmit
+	 */
 	private static void sendInfo(Map<PlayerId, Player> players, String info) {
-		for (PlayerId playerId : PlayerId.ALL)
-			players.get(playerId).receiveInfo(info);
+		PlayerId.ALL.stream().forEach((PlayerId playerId) -> players.get(playerId).receiveInfo(info));
 	}
 
+	/**
+	 * Updates every player's state for them to witness changes in-game
+	 * 
+	 * @param players
+	 * 				The map linking players to their identifier
+	 * 
+	 * @param gameState
+	 * 				The current game's state
+	 */
 	private static void updatePlayerStates(Map<PlayerId, Player> players, GameState gameState) {
-		for (PlayerId playerId : PlayerId.ALL)
-			players.get(playerId).updateState(gameState, gameState.playerState(playerId));
+		PlayerId.ALL.stream().forEach((PlayerId playerId) -> players.get(playerId).updateState(gameState, gameState.playerState(playerId)));
 	}
 }
