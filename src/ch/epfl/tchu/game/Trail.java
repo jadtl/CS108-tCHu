0364da3faaca1dfb779c8b01a41120862bec8edc
Jad Tala <JadTala1621@gmail.com>
2021-04-15 12:31:54 +0000

    Removed unused trail imports

--- src/ch/epfl/tchu/game/Trail.java
+++ src/ch/epfl/tchu/game/Trail.java
@@ -1,9 +1,7 @@
 package ch.epfl.tchu.game;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
