bd678d9d3eed00cddbd4b3c701a50b32973a451b
Jad Tala <JadTala1621@gmail.com>
2021-04-15 11:37:16 +0000

    Trail.longest() all with streams :)

--- src/ch/epfl/tchu/game/Trail.java
+++ src/ch/epfl/tchu/game/Trail.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * A Trail between two stations in the network
@@ -14,6 +16,7 @@ import java.util.Map;
 public final class Trail {
     private final Station station1;
     private final Station station2;
+    private final List<Route> routes; 
     private final int length;
 
     /**
@@ -28,10 +31,13 @@ public final class Trail {
      * @param length 
      *        The length of the trail
      */
-    private Trail(Station station1, Station station2, int length) {
+    private Trail(Station station1, Station station2, List<Route> routes) {
         this.station1 = station1;
         this.station2 = station2;
-        this.length = length;
+        this.routes = routes;
+        this.length = routes.stream()
+        .map(r -> r.length())
+        .reduce(0, Integer::sum);
     }
 
     /**
@@ -43,54 +49,50 @@ public final class Trail {
      * @return a trail with the maximum length for the given routes
      */
     public static Trail longest(List<Route> routes) {
-        List<Trail> toExtendTrails = new ArrayList<>();
-        // A List that stores trails that cannot be extended further
+        // Filling the list with starting trails from routes
+        List<Trail> toExtendTrails = routes.stream()
+        .flatMap(r -> Stream.of(new Trail(r.station1(), r.station2(), List.of(r)), new Trail(r.station2(), r.station1(), List.of(r))))
+        .collect(Collectors.toList());
+
+        // A list that stores trails that cannot be extended further
         List<Trail> deadEndTrails = new ArrayList<>();
-        // A Map that links a trail to its corresponding set of routes
-        Map<Trail, List<Route>> associatedRoutes = new HashMap<>();
 
-        // Filling the list and the map with starting elements from routes
-        for (Route route : routes) {
-            toExtendTrails.add(new Trail(route.station1(), route.station2(), route.length()));
-            associatedRoutes.put(toExtendTrails.get(toExtendTrails.size() - 1), List.of(route));
-            toExtendTrails.add(new Trail(route.station2(), route.station1(), route.length()));
-            associatedRoutes.put(toExtendTrails.get(toExtendTrails.size() - 1), List.of(route));
-        }
-
+        // A list that stores trails that just got extended
         List<Trail> updatedTrails = new ArrayList<>(toExtendTrails);
+
         // While at least one trail has been extended, search for further extensions
         while (!updatedTrails.isEmpty()) {
-            toExtendTrails = new ArrayList<>(updatedTrails);
+            // The trails that last got updated now must be extended if possible
+            toExtendTrails = List.copyOf(updatedTrails);
+
+            // The list of updated trails is cleared
             updatedTrails.clear();
-            // Looking for each trail if it can be extended
-            for (Trail toExtendTrail : toExtendTrails) {
-                // Removing from the candidate routes those which are already part of the trail
-                List<Route> candidateRoutes = new ArrayList<>(routes);
-                candidateRoutes.removeAll(associatedRoutes.get(toExtendTrail));
-                boolean canBeExtended = false;
-                // Looking for a route that can extend the trail
-                for (Route candidateRoute : candidateRoutes) {
-                    // A route can extend the trail iff one of its two stations is the last trail's station
-                    if (candidateRoute.stations().contains(toExtendTrail.station2())) {
-                        canBeExtended = true;
-                        // The new last trail station is the opposite station of the extending route
-                        updatedTrails.add(new Trail(toExtendTrail.station1(), candidateRoute.stationOpposite(toExtendTrail.station2()), 
-                        toExtendTrail.length() + candidateRoute.length()));
-                        // Updating the map with the new trail we've discovered
-                        List<Route> updatedRoutes = new ArrayList<>(associatedRoutes.get(toExtendTrail));
-                        updatedRoutes.add(candidateRoute);
-                        associatedRoutes.put(updatedTrails.get(updatedTrails.size() - 1), updatedRoutes);
-                    }
-                }
-                // If the current trail can't be extended, then it's a dead end
-                if (!canBeExtended) deadEndTrails.add(toExtendTrail);
-            }
+
+            // Iterating through the trails to be extended
+            toExtendTrails.stream().forEach(t -> {
+                // Removing from the routes list already used routes as well as the ones that
+                // do not contain the current trail's second station
+                List<Route> eligibleRoutes = routes.stream()
+                .filter(r -> !t.routes.contains(r) && r.stations().contains(t.station2()))
+                .collect(Collectors.toList());
+
+                if (eligibleRoutes.isEmpty())
+                    // If no eligible route exist, the current trail is a dead end
+                    deadEndTrails.add(t);
+                else
+                    // For every eligible route, a new longer trail is added to the updates
+                    eligibleRoutes.stream().forEach(r -> {
+                        List<Route> updatedRoutes = new ArrayList<Route>(t.routes);
+                        updatedRoutes.add(r);
+                        updatedTrails.add(new Trail(t.station1(), r.stationOpposite(t.station2()), updatedRoutes));
+                    }) ;
+            });
         }
 
         // Figuring the longest trail among the found dead ends
         return deadEndTrails.stream()
         .max((t1, t2) -> Integer.compare(t1.length(), t2.length()))
-        .orElse(new Trail(null, null, 0));
+        .orElse(new Trail(null, null, List.of()));
     }
 
     /**
