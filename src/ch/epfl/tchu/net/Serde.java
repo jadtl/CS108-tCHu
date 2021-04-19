33dd068cda86a37205c37b9b49fa15df12f7523c
Jad Tala <JadTala1621@gmail.com>
2021-04-19 23:21:47 +0000

    Documented the serde class

--- src/ch/epfl/tchu/net/Serde.java
+++ src/ch/epfl/tchu/net/Serde.java
@@ -10,36 +10,48 @@ import java.util.stream.Collectors;
 import ch.epfl.tchu.SortedBag;
 
 /**
+ * A serializer-deserializer
  * 
  * @param <T>
- * 
+ *        The type to serialize-deserialize
+ *
+ * @author Sofiya Malamud (313789)
+ * @author Jad Tala (310821)
  */
 public interface Serde<T> {
   /**
+   * Serializes a given object
    * 
    * @param toSerialize
+   *        The object to serialize
    * 
-   * @return
+   * @return the corresponding serialized string of the object
    */
   String serialize(T toSerialize);
 
   /**
+   * Deserializes an object using its corresponding string
    * 
    * @param toDeserialize
+   *        The string to deserialize
    * 
-   * @return
+   * @return the object corresponding to the given string
    */
   T deserialize(String toDeserialize);
 
   /**
+   * Returns a serde using the given serialize and deserialize functions
    * 
    * @param <T>
+   *        The type of the resulting serde
    * 
    * @param serializeFunction
+   *        The serdes's serialize function
    * 
    * @param deserializeFunction
+   *        The serde's serialize function
    * 
-   * @return
+   * @return a serde using the given serialize and deserialize functions
    */
   static <T> Serde<T> of(Function<T, String> serializeFunction, Function<String, T> deserializeFunction) {
     return new Serde<T>() {
@@ -56,12 +68,15 @@ public interface Serde<T> {
   }
 
   /**
+   * Returns a serde using the given list of all values of a set of enumerated values
    * 
    * @param <T>
+   *        The type of values
    * 
    * @param all
+   *        The list of all values of a set of enumerated values
    * 
-   * @return
+   * @return a serde using the given list of all values of a set of enumerated values
    */
   static <T> Serde<T> oneOf(List<T> all) {
     return Serde.of(
@@ -71,14 +86,20 @@ public interface Serde<T> {
   }
 
   /**
+   * Returns a serde able to serialize-deserialize a list of values
+   * serialized-deserialized by the given serde
    * 
    * @param <T>
+   *        The value type of the given serde
    * 
    * @param serde
+   *        The serde
    * 
    * @param delimiter
+   *        The delimiting character
    * 
-   * @return
+   * @return a serde able to serialize-deserialize a list of values
+   * serialized-deserialized by the given serde
    */
   static <T> Serde<List<T>> listOf(Serde<T> serde, String delimiter) {
     return Serde.of(
@@ -96,14 +117,20 @@ public interface Serde<T> {
   }
 
   /**
+   * Returns a serde able to serialize-deserialize a list of values
+   * serialized-deserialized by the given serde
    * 
    * @param <T>
+   *        The value type of the given serde
    * 
    * @param serde
+   *        The serde
    * 
    * @param delimiter
+   *        The delimiting character
    * 
-   * @return
+   * @return a serde able to serialize-deserialize a list of values
+   * serialized-deserialized by the given serde
    */
   static <T extends Comparable<T>> Serde<SortedBag<T>> bagOf(Serde<T> serde, String delimiter) {
     return Serde.of(
