aa1e7672f5511bf6865f4f8c5efc3f8b7ee849c9
Jad Tala <JadTala1621@gmail.com>
2021-04-20 00:29:12 +0000

    Tested basic serdes

---
+++ test/ch/epfl/tchu/net/SerdeTest.java
@@ -0,0 +1,52 @@
+package ch.epfl.tchu.net;
+
+import org.junit.jupiter.api.Test;
+
+import ch.epfl.tchu.game.Card;
+import ch.epfl.tchu.game.Player;
+import ch.epfl.tchu.game.PlayerId;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SerdeTest {
+  @Test
+  void integerSerdeWorks() {
+    Integer toSerialize = 42;
+    assertEquals("42", Serdes.INTEGER.serialize(toSerialize));
+    assertEquals(toSerialize, Serdes.INTEGER.deserialize(Serdes.INTEGER.serialize(toSerialize)));
+  }
+
+  @Test
+  void stringSerdeWorks() {
+    String toSerialize = "Imagine je fonctionne";
+    assertEquals("SW1hZ2luZSBqZSBmb25jdGlvbm5l", Serdes.STRING.serialize(toSerialize));
+    assertEquals(toSerialize, Serdes.STRING.deserialize(Serdes.STRING.serialize(toSerialize)));
+  }
+
+  @Test
+  void playerIdSerdeWorks() {
+    PlayerId toSerialize = PlayerId.PLAYER_2;
+    assertEquals("1", Serdes.PLAYER_ID.serialize(toSerialize));
+    assertEquals(toSerialize, Serdes.PLAYER_ID.deserialize(Serdes.PLAYER_ID.serialize(toSerialize)));
+  }
+
+  @Test
+  void turnKindSerdeWorks() {
+    Player.TurnKind toSerialize = Player.TurnKind.CLAIM_ROUTE;
+    assertEquals("2", Serdes.TURN_KIND.serialize(toSerialize));
+    assertEquals(toSerialize, Serdes.TURN_KIND.deserialize(Serdes.TURN_KIND.serialize(toSerialize)));
+  }
+
+  @Test
+  void cardSerdeWorks() {
+    Card toSerialize = Card.LOCOMOTIVE;
+    assertEquals("8", Serdes.CARD.serialize(toSerialize));
+    assertEquals(toSerialize, Serdes.CARD.deserialize(Serdes.CARD.serialize(toSerialize)));
+  }
+
+  @Test
+  void routeSerdeWorks() {
+
+  }
+}
