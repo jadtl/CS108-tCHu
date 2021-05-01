ca228394a9416248621afc46e950c9630c4ffd61
Jad Tala <JadTala1621@gmail.com>
2021-05-01 20:05:07 +0000

    Map view creator half implemented

---
+++ src/ch/epfl/tchu/gui/MapViewCreator.java
@@ -0,0 +1,100 @@
+package ch.epfl.tchu.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.w3c.dom.css.Rect;
+
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.Card;
+import ch.epfl.tchu.game.ChMap;
+import ch.epfl.tchu.game.PlayerId;
+import ch.epfl.tchu.game.Route;
+import ch.epfl.tchu.game.Route.Level;
+import ch.epfl.tchu.gui.ActionHandlers.ChooseCardsHandler;
+import ch.epfl.tchu.gui.ActionHandlers.ClaimRouteHandler;
+import javafx.beans.property.ObjectProperty;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * 
+ * 
+ * @author Sofiya Malamud (313789)
+ * @author Jad Tala (310821)
+ */
+class MapViewCreator {
+  /**
+   * The class is non-instanciable
+   */
+  private MapViewCreator() {}
+
+  /**
+   * 
+   * @param observableGameState
+   * 
+   * @param claimRouteHandlerProperty
+   * 
+   * @param cardChooser
+   * 
+   * @return
+   */
+  public static Pane createMapView(ObservableGameState observableGameState, 
+    ObjectProperty<ClaimRouteHandler> claimRouteHandlerProperty, CardChooser cardChooser) {
+      List<Node> allRouteGroups = new ArrayList<Node>();
+      ChMap.routes().stream().forEach(r -> allRouteGroups.add(createRouteGroup(r, observableGameState.routesOwnershipProperty(r).get())));
+
+      ImageView background = new ImageView();
+
+      Pane mapView = new Pane(background);
+      mapView.getChildren().addAll(allRouteGroups);
+      mapView.getStylesheets().addAll(List.of("map.css", "colors.css"));
+      
+      return mapView;
+  }
+
+  private static Group createRouteGroup(Route route, PlayerId owner) {
+    Rectangle carRectangle = new Rectangle(36, 12);
+    carRectangle.getStyleClass().add("filled");
+    Circle carCircle1 = new Circle(12, 6, 0);
+    carCircle1.getStyleClass().add("filled");
+    Circle carCircle2 = new Circle(24, 6, 0);
+    carCircle2.getStyleClass().add("filled");
+    Group car = new Group(List.of(carRectangle, carCircle1, carCircle2));
+    car.getStyleClass().add("car");
+
+    Rectangle track = new Rectangle(36, 12);
+    track.getStyleClass().add("track");
+    track.getStyleClass().add("filled");
+
+    List<Node> tiles = new ArrayList<Node>();
+    for (int i = 1; i <= route.length(); i++) {
+      Group tile = new Group(List.of(car, track));
+      tile.setId(String.join("_", List.of(route.id(), String.valueOf(i))));
+      tiles.add(tile);
+    }
+
+    Group routeGroup = new Group(tiles);
+    routeGroup.setId(route.id());
+    routeGroup.getStyleClass().add("route");
+    if (route.level().equals(Level.UNDERGROUND)) routeGroup.getStyleClass().add("UNDERGROUND");
+    if (Objects.isNull(route.color())) routeGroup.getStyleClass().add("NEUTRAL");
+    if (!Objects.isNull(owner)) routeGroup.getStyleClass().add(owner.toString());
+
+    return routeGroup;
+  }
+
+  /**
+   * 
+   * 
+   */
+  @FunctionalInterface
+  interface CardChooser {
+    void chooseCards(List<SortedBag<Card>> options, ChooseCardsHandler handler);
+  }
+}
