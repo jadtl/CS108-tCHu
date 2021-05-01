50d645789616ef31cc10bc82d2c632d13c89dbc2
Jad Tala <JadTala1621@gmail.com>
2021-05-01 16:21:15 +0000

    Implemented action handlers

---
+++ src/ch/epfl/tchu/gui/ActionHandlers.java
@@ -0,0 +1,59 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.game.Card;
+import ch.epfl.tchu.game.Route;
+import ch.epfl.tchu.game.Ticket;
+
+/**
+ * 
+ * 
+ * @author Sofiya Malamud (313789)
+ * @author Jad Tala (310821)
+ */
+public interface ActionHandlers {
+  /**
+   * 
+   * 
+   */
+  @FunctionalInterface
+  public interface DrawTicketsHandler {
+    void onDrawTickets();
+  }
+
+  /**
+   * 
+   * 
+   */
+  @FunctionalInterface
+  public interface DrawCardHandler {
+    void onDrawCard(int slot);
+  }
+
+  /**
+   * 
+   * 
+   */
+  @FunctionalInterface
+  public interface ClaimRouteHandler {
+    void onClaimRoute(Route route, SortedBag<Card> claimCards);
+  }
+
+  /**
+   * 
+   * 
+   */
+  @FunctionalInterface
+  public interface ChooseTicketsHandler {
+    void onChooseTickets(SortedBag<Ticket> chosenTickets);
+  }
+
+  /**
+   * 
+   * 
+   */
+  @FunctionalInterface
+  public interface ChooseCardsHandler {
+    void onChooseCards(SortedBag<Card> chosenCards);
+  }
+}
