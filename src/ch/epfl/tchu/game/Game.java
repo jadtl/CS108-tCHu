46bc68457228fc96726ff6de594cafd8420f8b4a
Jad Tala <JadTala1621@gmail.com>
2021-05-07 20:16:19 +0000

    Game class fixes

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -23,6 +23,11 @@ public final class Game {
 	private static final int DRAW_CARDS_TIMES = 2;
 
 	/**
+	 * The class is non-instanciable
+	 */
+	private Game() {}
+
+	/**
 	 * Makes the given players play a game of tCHu, whose names and identifiers figure in the given maps
 	 * 
 	 * @param players
@@ -59,9 +64,10 @@ public final class Game {
 		.collect(Collectors.toMap(p -> p, p -> new Info(playerNames.get(p))));
 		
 		// Informing the players which plays first
-		sendInfo(players,playerInfos.get(gameState.currentPlayerId()).willPlayFirst());
+		sendInfo(players, playerInfos.get(gameState.currentPlayerId()).willPlayFirst());
 
 		// Drawing initial tickets and asking the player which they want to keep
+		// TODO: Tell TA that couldn't get given solution to work
 		for (int i = 0; i < PlayerId.COUNT; i++) {
 			SortedBag<Ticket> initialTickets = gameState.withoutTopTickets(Constants.INITIAL_TICKETS_COUNT * i).topTickets(Constants.INITIAL_TICKETS_COUNT);
 			players.get(PlayerId.ALL.get(i)).setInitialTicketChoice(initialTickets);
@@ -80,18 +86,22 @@ public final class Game {
 		 * The Middle Game *
 		 * * * * * * * * * */
 
+		// TODO: Tell TA that currentPlayerInfo only accesses a map value so doesn't calculate anything
 		// Keep playing until the last turn is played
 		while (Objects.isNull(gameState.lastPlayer()) || gameState.currentPlayerId() != gameState.lastPlayer()) {
 			// Informing that the last turn begins if the conditions are met
 			if (gameState.lastTurnBegins()) sendInfo(players, playerInfos.get(gameState.currentPlayerId()).lastTurnBegins(gameState.currentPlayerState().carCount()));
 			
+			// TODO: Fix the fact that the first player's turn passes
 			// Switching the game state to the next turn
 			gameState = gameState.forNextTurn();
+		
+			Info currentPlayerInfo = playerInfos.get(gameState.currentPlayerId());
 
 			// Announcing which player plays this turn
 			Player currentPlayer = players.get(gameState.currentPlayerId());
 			updatePlayerStates(players, gameState);
-			sendInfo(players, playerInfos.get(gameState.currentPlayerId()).canPlay());
+			sendInfo(players, currentPlayerInfo.canPlay());
 
 			// Asks what the player's next move will be
 			TurnKind playerMove = currentPlayer.nextTurn();
@@ -100,7 +110,7 @@ public final class Game {
 			switch (playerMove) {
 				// The player decides to draw tickets
 				case DRAW_TICKETS:
-					sendInfo(players, playerInfos.get(gameState.currentPlayerId()).drewTickets(Constants.IN_GAME_TICKETS_COUNT));
+					sendInfo(players, currentPlayerInfo.drewTickets(Constants.IN_GAME_TICKETS_COUNT));
 				
 					// Asking the current player which tickets they want to keep
 					SortedBag<Ticket> chosenTickets = currentPlayer.chooseTickets(gameState.topTickets(Constants.IN_GAME_TICKETS_COUNT));
@@ -109,7 +119,7 @@ public final class Game {
 					gameState = gameState.withChosenAdditionalTickets(gameState.topTickets(Constants.IN_GAME_TICKETS_COUNT), chosenTickets);
 					
 					// Informing of the number of tickets drawn by the current player
-					sendInfo(players, playerInfos.get(gameState.currentPlayerId()).keptTickets(chosenTickets.size()));
+					sendInfo(players, currentPlayerInfo.keptTickets(chosenTickets.size()));
 					break;
 
 				// The player decides to draw cards
@@ -121,15 +131,14 @@ public final class Game {
 						// Determines which card the player wants to draw
 						int drawSlot = currentPlayer.drawSlot();
 
+						gameState = gameState.withCardsDeckRecreatedIfNeeded(rng);
 						// The player draws a deck card
 						if (drawSlot == Constants.DECK_SLOT) {
 							// Updates the game state to a state where a top deck card has been drawn
-							gameState = gameState
-							.withCardsDeckRecreatedIfNeeded(rng)
-							.withBlindlyDrawnCard();
+							gameState = gameState.withBlindlyDrawnCard();
 
 							// Informing that the current play drew a deck card
-							sendInfo(players, playerInfos.get(gameState.currentPlayerId()).drewBlindCard());
+							sendInfo(players, currentPlayerInfo.drewBlindCard());
 						}
 						// The player draws a face-up card
 						else {
@@ -137,12 +146,10 @@ public final class Game {
 							Card drawnCard = gameState.cardState().faceUpCard(drawSlot);
 
 							// Updates the game state to a state where the face-up card was drawn
-							gameState = gameState
-							.withCardsDeckRecreatedIfNeeded(rng)
-							.withDrawnFaceUpCard(drawSlot);
+							gameState = gameState.withDrawnFaceUpCard(drawSlot);
 
 							// Informing that the current player drew the given face-up card
-							sendInfo(players, playerInfos.get(gameState.currentPlayerId()).drewVisibleCard(drawnCard));
+							sendInfo(players, currentPlayerInfo.drewVisibleCard(drawnCard));
 						}
 					}
 					break;
@@ -155,24 +162,25 @@ public final class Game {
 					// The player attempts to claim a tunnel
 					if (claimedRoute.level().equals(Level.UNDERGROUND)) {
 						// Informs that the current player is trying to claim a Tunnel
-						sendInfo(players, playerInfos.get(gameState.currentPlayerId()).attemptsTunnelClaim(claimedRoute, initialClaimCards));
+						sendInfo(players, currentPlayerInfo.attemptsTunnelClaim(claimedRoute, initialClaimCards));
 
 						// Drawing the additional tunnel cards
-						SortedBag.Builder<Card> additionalCards = new SortedBag.Builder<Card>();
+						SortedBag.Builder<Card> additionalCardsBuilder = new SortedBag.Builder<Card>();
 						for (int i = 0; i < Constants.ADDITIONAL_TUNNEL_CARDS; i++) {
 							gameState = gameState.withCardsDeckRecreatedIfNeeded(rng);
-							additionalCards.add(gameState.topCard());
+							additionalCardsBuilder.add(gameState.topCard());
 							gameState = gameState.withoutTopCard();
 						}
+						SortedBag<Card> additionalCards = additionalCardsBuilder.build();
 
 						// The additional cards are added to the pile of discards
-						gameState = gameState.withMoreDiscardedCards(additionalCards.build());
+						gameState = gameState.withMoreDiscardedCards(additionalCards);
 						
 						// Computing the additional claim cards that the additional tunnel cards imply
-						int additionalClaimCardsCount = claimedRoute.additionalClaimCardsCount(initialClaimCards, additionalCards.build());
+						int additionalClaimCardsCount = claimedRoute.additionalClaimCardsCount(initialClaimCards, additionalCards);
 
 						// Informing that the current player drew additional cards to try and claim the tunnel
-						sendInfo(players, playerInfos.get(gameState.currentPlayerId()).drewAdditionalCards(additionalCards.build(), additionalClaimCardsCount));
+						sendInfo(players, currentPlayerInfo.drewAdditionalCards(additionalCards, additionalClaimCardsCount));
 						
 						// The player does not need to use any additional cards to claim the tunnel
 						if (additionalClaimCardsCount == 0) {
@@ -180,12 +188,12 @@ public final class Game {
 							.withClaimedRoute(claimedRoute, initialClaimCards);
 
 							// Informing that the current player claimed the tunnel using only the initial claim cards
-							sendInfo(players, playerInfos.get(gameState.currentPlayerId()).claimedRoute(claimedRoute, initialClaimCards));
+							sendInfo(players, currentPlayerInfo.claimedRoute(claimedRoute, initialClaimCards));
 						}
 						else {
 							// Computing the list of possible additional claim cards the player can choose to try and claim the tunnel
 							List<SortedBag<Card>> possibleAdditionalClaimCards = gameState.currentPlayerState().possibleAdditionalCards
-							(additionalClaimCardsCount, initialClaimCards, additionalCards.build());
+							(additionalClaimCardsCount, initialClaimCards, additionalCardsBuilder.build());
 
 							// The player has at least one set of additional claim cards to play
 							if (possibleAdditionalClaimCards.size() > 0) {
@@ -195,20 +203,20 @@ public final class Game {
 								// The player cannot or doesn't want to claim the tunnel
 								if (chosenAdditionalClaimCards.isEmpty()) {
 									// Informing that the current player did not claim the tunnel
-									sendInfo(players, playerInfos.get(gameState.currentPlayerId()).didNotClaimRoute(claimedRoute));
+									sendInfo(players, currentPlayerInfo.didNotClaimRoute(claimedRoute));
 								}
 								else {
 									// Adding the additional claim cards to the initial cards
-									SortedBag<Card> finalClaimCards = new SortedBag.Builder<Card>().add(initialClaimCards).add(chosenAdditionalClaimCards).build();
+									SortedBag<Card> finalClaimCards = initialClaimCards.union(chosenAdditionalClaimCards);
 									gameState = gameState
 									.withClaimedRoute(claimedRoute, finalClaimCards);
 									// Informing that the current player claimed the tunnel using additional cards
-									sendInfo(players, playerInfos.get(gameState.currentPlayerId()).claimedRoute(claimedRoute, finalClaimCards));
+									sendInfo(players, currentPlayerInfo.claimedRoute(claimedRoute, finalClaimCards));
 								}
 							}
 							else {
 								// Informing that the current player didn't claim the route
-								sendInfo(players, playerInfos.get(gameState.currentPlayerId()).didNotClaimRoute(claimedRoute));
+								sendInfo(players, currentPlayerInfo.didNotClaimRoute(claimedRoute));
 							}
 						}
 					}
@@ -216,7 +224,7 @@ public final class Game {
 					else if (claimedRoute.level().equals(Level.OVERGROUND)) {
 						gameState = gameState.withClaimedRoute(claimedRoute, initialClaimCards);
 						// Informing that the current player claimed the route
-						sendInfo(players, playerInfos.get(gameState.currentPlayerId()).claimedRoute(claimedRoute, initialClaimCards));
+						sendInfo(players, currentPlayerInfo.claimedRoute(claimedRoute, initialClaimCards));
 					}
 					break;
 			}
@@ -279,7 +287,7 @@ public final class Game {
 	 * 				The information to transmit
 	 */
 	private static void sendInfo(Map<PlayerId, Player> players, String info) {
-		PlayerId.ALL.stream().forEach((PlayerId playerId) -> players.get(playerId).receiveInfo(info));
+		PlayerId.ALL.forEach((PlayerId playerId) -> players.get(playerId).receiveInfo(info));
 	}
 
 	/**
@@ -292,6 +300,6 @@ public final class Game {
 	 * 				The current game's state
 	 */
 	private static void updatePlayerStates(Map<PlayerId, Player> players, GameState gameState) {
-		PlayerId.ALL.stream().forEach((PlayerId playerId) -> players.get(playerId).updateState(gameState, gameState.playerState(playerId)));
+		PlayerId.ALL.forEach((PlayerId playerId) -> players.get(playerId).updateState(gameState, gameState.playerState(playerId)));
 	}
 }
