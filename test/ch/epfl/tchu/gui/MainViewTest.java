60e55d218c33cc7eb59ad40d497ec2be456b6b68
Jad Tala <JadTala1621@gmail.com>
2021-05-08 10:54:26 +0000

    Added view info test

--- test/ch/epfl/tchu/gui/MainViewTest.java
+++ test/ch/epfl/tchu/gui/MainViewTest.java
@@ -22,10 +22,13 @@ import ch.epfl.tchu.gui.ActionHandlers.DrawTicketsHandler;
 import javafx.application.Application;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public final class MainViewTest extends Application {
@@ -58,8 +61,16 @@ public final class MainViewTest extends Application {
     Node handView = DecksViewCreator
       .createHandView(gameState);
 
+      Map<PlayerId, String> playerNames =
+      Map.of(PlayerId.PLAYER_1, "Ada", PlayerId.PLAYER_2, "Charles");
+    ObservableList<Text> infos = FXCollections.observableArrayList(
+      new Text("Première information.\n"),
+      new Text("\nSeconde information.\n"));
+    Node infoView = InfoViewCreator
+      .createInfoView(PlayerId.PLAYER_1, playerNames, gameState, infos);
+
     BorderPane mainPane =
-      new BorderPane(mapView, null, cardsView, handView, null);
+      new BorderPane(mapView, null, cardsView, handView, infoView);
     primaryStage.setScene(new Scene(mainPane));
     primaryStage.show();
 
