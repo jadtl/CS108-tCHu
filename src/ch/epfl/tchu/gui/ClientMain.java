385dc4e251993a8316d1ebe0a37a288520ca5e8a
sofiya-hub <sofiya.malamud@epfl.ch>
2021-05-23 21:44:08 +0200

    comitted

--- src/ch/epfl/tchu/gui/ClientMain.java
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -1,6 +1,40 @@
 // TODO: Client main Javadoc
 package ch.epfl.tchu.gui;
 
-public class ClientMain {
+import java.util.List;
+import java.util.Map;
+
+import ch.epfl.tchu.game.Player;
+import ch.epfl.tchu.game.PlayerId;
+import ch.epfl.tchu.net.RemotePlayerClient;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import static ch.epfl.tchu.game.PlayerId.*;
+
+public class ClientMain extends Application{
   
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+    @Override
+
+    public void start(Stage primaryStage){
+        
+       List<String> args = this.getParameters().getRaw();
+       String hostName = "localhost";
+       int port = 5108;
+       if(args.size() >= 2){
+         hostName = args.get(0);
+         port = Integer.valueOf(args.get(1));
+       }
+       Map<PlayerId, String> names =
+        Map.of(PLAYER_1, "Ada", PLAYER_2, "Charles");
+      Map<PlayerId, Player> players =
+        Map.of(PLAYER_1, new GraphicalPlayerAdapter(),
+           PLAYER_2, new GraphicalPlayerAdapter());
+      RemotePlayerClient remotePlayerClient = new RemotePlayerClient(new GraphicalPlayerAdapter(), hostName, port);
+
+      new Thread (() -> remotePlayerClient.run()).start();
+    }
 }
